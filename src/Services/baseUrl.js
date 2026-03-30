14dc21fd37d474fe37bdf0f4d538208c459ad195
Jithin pm <147484859+jithin-pm@users.noreply.github.com>
2026-03-30 12:44:03 +0530

    url fix

--- src/Services/baseUrl.js
+++ src/Services/baseUrl.js
@@ -1,3 +1,3 @@
 /* export const BASE_URL = 'https://lunarsenterprises.com:6060' */
- export const BASE_URL = 'http://localhost:6060' 
-/* export const BASE_URL = 'https://yashfifoundationbackend.yashfifoundation.org' */
+/*  export const BASE_URL = 'http://localhost:6060'  */
+ export const BASE_URL = 'https://yashfifoundationbackend.yashfifoundation.org' 
